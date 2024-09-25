d34480e14f2ab427a3994359b5e93e7e0bade962
Jerry <jerryyangshaoern@gmail.com>
2024-09-26 00:06:43 +0800

    black lint

--- src/similar_ans_gen.py
+++ src/similar_ans_gen.py
@@ -12,16 +12,19 @@ from groq import Groq
 
 ## load evv variables
 from dotenv import load_dotenv
+
 load_dotenv()
-GROQ_API_KEY       = os.environ["GROQ_API_KEY"]
-CHAT_MODEL         = "llama3-70b-8192"
+GROQ_API_KEY = os.environ["GROQ_API_KEY"]
+CHAT_MODEL = "llama3-70b-8192"
 client = Groq()
 
 import logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-GENERATE_SIMILAR_ANSWER_PROMPT = \
-'''You are an expert in linguistic variation and medical communication. 
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+GENERATE_SIMILAR_ANSWER_PROMPT = """You are an expert in linguistic variation and medical communication. 
 Your task is to generate 5 similar, yet distinct, answers for a given question-answer pair. 
 These answers should maintain the core meaning and accuracy of the original answer but vary in phrasing, word choice, and sentence structure to provide different ways of expressing the same response. 
 The variations should be natural, contextually appropriate, and reflect the kind of nuanced responses that different knowledgeable individuals might provide.
@@ -46,10 +49,9 @@ The returning format should be like this:
 
 Before returning the answer, ensure and double check that the answer is in accordance to the format above.
 Do not return things like ** Variation 1 ** , ** Response 1 **
-'''
+"""
 
-GENERATE_POOR_ANSWER_PROMPT = \
-'''You are an expert in generating misleading and irrelevant content. 
+GENERATE_POOR_ANSWER_PROMPT = """You are an expert in generating misleading and irrelevant content. 
 Your task is to produce 5 responses for a given question-answer pair that are completely opposite, incorrect, and irrelevant to the original answer.
 These responses should disregard the core meaning and accuracy of the original answer, instead focusing on providing misleading, confusing, or nonsensical information.
 The variations should be inconsistent, contextually inappropriate, and reflect the kind of responses that lack knowledge or understanding.
@@ -74,7 +76,8 @@ Before returning the answer, ensure and double check that the answer is in accor
 Ensure and double-check that the generated answers are incoherent, misleading, and do not align with the original answer in any meaningful way.
 Do not return things like ** Variation 1 ** , ** Response 1 ** or any other format similar to this. Strictly follow the format above.
 
-'''
+"""
+
 
 def extract_answer(input_string):
     """
@@ -98,21 +101,21 @@ def extract_answer(input_string):
     """
     # Find the start and end indices of the JSON data within the input string
     # Assuming the JSON data starts with '{' and ends with '}'
-    json_start = input_string.find('{')
-    json_end = input_string.rfind('}') + 1
-    
+    json_start = input_string.find("{")
+    json_end = input_string.rfind("}") + 1
+
     # If either the start or end index is not found, raise an error
     if json_start == -1 or json_end == -1:
         raise ValueError("Invalid input: No JSON data found.")
 
     # Extract the substring that potentially contains the JSON data
     json_data = input_string[json_start:json_end]
-    
+
     try:
         # Attempt to convert the JSON string to a Python dictionary
         data_dict = json.loads(json_data)
         return data_dict
-    
+
     except json.JSONDecodeError:
         # If JSON decoding fails, search for a JSON object containing the 'questions' key
         # Using regex to match a pattern that includes the 'questions' key
@@ -127,8 +130,11 @@ def extract_answer(input_string):
 
         # If no valid JSON is found, the function will Log an error
         else:
-            logging.error("No dictionary with 'questions' as a key found in this input string. Error by LLM")
+            logging.error(
+                "No dictionary with 'questions' as a key found in this input string. Error by LLM"
+            )
             return {"error": "No dictionary with questions found"}
+
 
 def generate_sim_answers(qa_pair, answer_prompt, client):
     """
@@ -150,14 +156,14 @@ def generate_sim_answers(qa_pair, answer_prompt, client):
     Raises:
         KeyError: If the 'error' key is found in the response dictionary.
     """
-    
-    qn = qa_pair['Question']
-    ans = qa_pair['Answer']
+
+    qn = qa_pair["Question"]
+    ans = qa_pair["Answer"]
 
     # Define a Pydantic model for the expected answer structure
     class AnswerList(BaseModel):
         answers: list = Field(description="Similar answer to the provided answer")
-    
+
     # Initialize a JSON output parser using the defined Pydantic model
     parser = JsonOutputParser(pydantic_object=AnswerList)
 
@@ -166,20 +172,15 @@ def generate_sim_answers(qa_pair, answer_prompt, client):
         template=answer_prompt,
         input_variables=["answer", "question"],
         partial_variables={"format_instructions": parser.get_format_instructions()},
-    ) 
-    
+    )
+
     # Format the final prompt with the actual text data and question list
     final_prompt = prompt.format(question=qn, answer=ans)
 
     # Generate the completion by interacting with the language model API
     completion = client.chat.completions.create(
         model=CHAT_MODEL,
-        messages=[
-            {
-                "role": "user",
-                "content": final_prompt
-            }
-        ],
+        messages=[{"role": "user", "content": final_prompt}],
         temperature=0,  # Control the randomness of the output (lower means less random)
         max_tokens=1024,  # Limit the response length
         top_p=1,  # Nucleus sampling parameter (1 means only the most likely tokens are considered)
@@ -188,20 +189,21 @@ def generate_sim_answers(qa_pair, answer_prompt, client):
     )
 
     # Initialize an empty string to accumulate the response content
-    answer = ''''''
+    answer = """"""
     for chunk in completion:
         # Append each chunk of content to the answer string
         answer += chunk.choices[0].delta.content or ""
-    
+
     # Extract the answers from the accumulated response content
     answer_dict = extract_answer(answer)
 
     # Log an error if the response contains an 'error' key
     if "error" in answer_dict:
         logging.error(f"{answer_dict['error']}")
-    
+
     # Return the dictionary containing the generated answers
     return answer_dict
+
 
 def generate_poor_answers(qa_pair, answer_prompt, client):
     """
@@ -223,14 +225,16 @@ def generate_poor_answers(qa_pair, answer_prompt, client):
     Raises:
         KeyError: If the 'error' key is found in the response dictionary.
     """
-    
-    qn = qa_pair['Question']
-    ans = qa_pair['Answer']
+
+    qn = qa_pair["Question"]
+    ans = qa_pair["Answer"]
 
     # Define a Pydantic model for the expected answer structure
     class AnswerList(BaseModel):
-        answers: list = Field(description="Answer completely different to the provided answer")
-    
+        answers: list = Field(
+            description="Answer completely different to the provided answer"
+        )
+
     # Initialize a JSON output parser using the defined Pydantic model
     parser = JsonOutputParser(pydantic_object=AnswerList)
 
@@ -239,20 +243,15 @@ def generate_poor_answers(qa_pair, answer_prompt, client):
         template=answer_prompt,
         input_variables=["answer", "question"],
         partial_variables={"format_instructions": parser.get_format_instructions()},
-    ) 
-    
+    )
+
     # Format the final prompt with the actual text data and question list
     final_prompt = prompt.format(question=qn, answer=ans)
 
     # Generate the completion by interacting with the language model API
     completion = client.chat.completions.create(
         model=CHAT_MODEL,
-        messages=[
-            {
-                "role": "user",
-                "content": final_prompt
-            }
-        ],
+        messages=[{"role": "user", "content": final_prompt}],
         temperature=0,  # Control the randomness of the output (lower means less random)
         max_tokens=1024,  # Limit the response length
         top_p=1,  # Nucleus sampling parameter (1 means only the most likely tokens are considered)
@@ -261,39 +260,38 @@ def generate_poor_answers(qa_pair, answer_prompt, client):
     )
 
     # Initialize an empty string to accumulate the response content
-    answer = ''''''
+    answer = """"""
     for chunk in completion:
         # Append each chunk of content to the answer string
         answer += chunk.choices[0].delta.content or ""
-    
+
     # Extract the answers from the accumulated response content
     answer_dict = extract_answer(answer)
 
     # Log an error if the response contains an 'error' key
     if "error" in answer_dict:
         logging.error(f"{answer_dict['error']}")
-    
+
     # Return the dictionary containing the generated answers
     return answer_dict
 
 
-
 if __name__ == "__main__":
     with open("../data/final_draft_2_pairs.json", "r", encoding="utf-8") as fin:
-        pairs = json.load(fin)    
+        pairs = json.load(fin)
 
     output_file = "../data/updated_pairs_2.json"
-    
+
     all_data = []
     for i in trange(220):
         dic = {}
-        dic['index'] = i
-        
-        sim_ans  = generate_sim_answers(pairs[i], GENERATE_SIMILAR_ANSWER_PROMPT, client)
+        dic["index"] = i
+
+        sim_ans = generate_sim_answers(pairs[i], GENERATE_SIMILAR_ANSWER_PROMPT, client)
         poor_ans = generate_poor_answers(pairs[i], GENERATE_POOR_ANSWER_PROMPT, client)
 
-        dic['Similar Answers'] = sim_ans['answers']
-        dic['Poor Answers'] = poor_ans['answers']
+        dic["Similar Answers"] = sim_ans["answers"]
+        dic["Poor Answers"] = poor_ans["answers"]
 
         all_data.append(dic)
 
@@ -301,9 +299,7 @@ if __name__ == "__main__":
         with open(output_file, "w", encoding="utf-8") as fout:
             json.dump(all_data, fout, ensure_ascii=False, indent=4)
 
-
         # account for rate limit
         if i > 0 and i % 11 == 0:
             print("Sleeping now...")
             time.sleep(62)
-            
